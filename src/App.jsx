dd2a9b408d3b471aabeb9e9bbfbb04e12dbb517a
Sourish M <sourish666@gmail.com>
2024-11-13 00:17:15 +0530

    Minor ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth0 } from "@auth0/auth0-react";
-import Navbar from './components/Navbar';
-import Dashboard from './dashboard/dashboard';
+// import Navbar from './components/Navbar';
+// import Dashboard from './dashboard/dashboard';
 import Home from './screens/Home';
 import Features from './screens/Features';
 import About from './screens/About';
 import Pricing from './screens/Pricing';
+import Navbar from './NavBar/Navbar';
 
 function App() {
-  const { isAuthenticated, isLoading } = useAuth0();
+  const { isLoading } = useAuth0();
 
   if (isLoading) {
     return (
@@ -29,16 +30,6 @@ function App() {
             <Route path="/features" element={<Features />} />
             <Route path="/about" element={<About />} />
             <Route path="/pricing" element={<Pricing />} />
-            <Route
-              path="/dashboard/*"
-              element={
-                isAuthenticated ? (
-                  <Dashboard />
-                ) : (
-                  <Navigate to="/" replace />
-                )
-              }
-            />
           </Routes>
         </div>
       </div>
